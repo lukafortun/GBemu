1d5b64433991984801b96c49cb6914e5b8e47cfa
Luka Fortun <ayn0s@pm.me>
2025-03-24 13:10:32 +0100

    added to cpu new registers for interuptions

--- include/cpu/cpu.h
+++ include/cpu/cpu.h
@@ -2,6 +2,12 @@
 #define CPU_H
 
 #include "../common.h"
+#include <stdbool.h>
+
+#define Z_FLAG 0x80  // Bit 7
+#define N_FLAG 0x40  // Bit 6
+#define H_FLAG 0x20  // Bit 5
+#define C_FLAG 0x10  // Bit 4
 
 // CPU registers
 typedef struct {
@@ -39,6 +45,11 @@ typedef struct {
 
     u16 sp; // Stack pointer
     u16 pc; // Program counter
+  
+    bool ime;       // Interrupt Master Enable 
+    uint8_t ie;     // Interrupt Enable Register 
+    uint8_t ir; // Interrupt Request Register 
+
 } CPU;
 
 
--- include/cpu/opcodes.h
+++ include/cpu/opcodes.h
@@ -2,7 +2,7 @@
 #define OPCODES_H
 
 #include "op_arith.h"
-#include "op_jump.h"
+#include "op_ctrlflow.h"
 #include "op_load.h"
 #include "op_misc.h"
 #include "op_rot_shift.h"
