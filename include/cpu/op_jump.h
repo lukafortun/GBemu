36d938dd5419b6f51d3344bd66434e6c95bb0b2c
Luka Fortun <ayn0s@pm.me>
2025-03-24 11:26:33 +0100

    added opcodes in commentary

--- include/cpu/op_jump.h
+++ include/cpu/op_jump.h
@@ -36,14 +36,14 @@ void jr_e(CPU *cpu, Memory *memory); // 0x18
 */
 
 // Conditional function call to the absolute address specified by the 16-bit operand nn, depending on the condition cc.
-void call_nz_nn(CPU *cpu, Memory *memory);
-void call_nc_nn(CPU *cpu, Memory *memory);
-void call_z_nn(CPU *cpu, Memory *memory);
-void call_c_nn(CPU *cpu, Memory *memory);
+void call_nz_nn(CPU *cpu, Memory *memory); // 0xC4
+void call_nc_nn(CPU *cpu, Memory *memory); // 0xD4
+void call_z_nn(CPU *cpu, Memory *memory); // 0xCC
+void call_c_nn(CPU *cpu, Memory *memory); // 0xDC
 
 
 // Unconditional function call to the absolute address specified by the 16-bit operand nn.
-void call_nn(CPU *cpu, Memory *memory);
+void call_nn(CPU *cpu, Memory *memory); // 0xDD
 
 
 /*
@@ -51,28 +51,28 @@ void call_nn(CPU *cpu, Memory *memory);
 */
 
 // Conditional return from a function, depending on the condition cc.
-void ret_nz(CPU *cpu, Memory *memory);
-void ret_nc(CPU *cpu, Memory *memory);
-void ret_z(CPU *cpu, Memory *memory);
-void ret_z(CPU *cpu, Memory *memory);
+void ret_nz(CPU *cpu, Memory *memory); // 0xC0
+void ret_nc(CPU *cpu, Memory *memory); // 0xD0
+void ret_z(CPU *cpu, Memory *memory); // 0xC8
+void ret_z(CPU *cpu, Memory *memory); // 0xD8
 
 // Unconditional return from a function.
-void ret(CPU *cpu, Memory *memory);
+void ret(CPU *cpu, Memory *memory); // 0xC9
 
 
 // Unconditional return from a function. Also enables interrupts by setting IME=1.
-void reti(CPU *cpu, Memory *memory);
+void reti(CPU *cpu, Memory *memory); // 0xD9
 
 
 // Unconditional function call to the absolute fixed address defined by the opcode.
-void rst_0x00(CPU *cpu, Memory *memory);
-void rst_0x10(CPU *cpu, Memory *memory);
-void rst_0x20(CPU *cpu, Memory *memory);
-void rst_0x30(CPU *cpu, Memory *memory);
-void rst_0x08(CPU *cpu, Memory *memory);
-void rst_0x18(CPU *cpu, Memory *memory);
-void rst_0x28(CPU *cpu, Memory *memory);
-void rst_0x38(CPU *cpu, Memory *memory);
+void rst_0x00(CPU *cpu, Memory *memory); // 0xC7
+void rst_0x10(CPU *cpu, Memory *memory); // 0xD7
+void rst_0x20(CPU *cpu, Memory *memory); // 0xE7
+void rst_0x30(CPU *cpu, Memory *memory); // 0xF7
+void rst_0x08(CPU *cpu, Memory *memory); // 0xCF
+void rst_0x18(CPU *cpu, Memory *memory); // 0xDF
+void rst_0x28(CPU *cpu, Memory *memory); // 0xEF
+void rst_0x38(CPU *cpu, Memory *memory); // 0xFF
 
 
 #endif
