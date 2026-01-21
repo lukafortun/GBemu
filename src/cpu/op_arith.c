4f5621af290f86936455fd688421ae5e0db1035c
Luka Fortun <ayn0s@pm.me>
2026-01-21 15:34:40 +0100

    modified some instruction

--- src/cpu/op_arith.c
+++ src/cpu/op_arith.c
@@ -148,7 +148,7 @@ void xor_l(CPU *cpu, Memory *memory) { xor_r(cpu, cpu->l); }; // 0xAD
 void xor_a(CPU *cpu, Memory *memory) { xor_r(cpu, cpu->a); }; // 0xAF
 
 // Performs a bitwise XOR operation between the 8-bit A register and data from the absolute address specified by the 16-bit register HL, and stores the result back into the A register.
-void xor_hl(CPU *cpu, Memory *memory){ xor_r(cpu, memory_read(memory, cpu->pc++));}; // 0xAE
+void xor_hl(CPU *cpu, Memory *memory){ xor_r(cpu, memory_read(memory, cpu->hl));}; // 0xAE
 
 // Performs a bitwise XOR operation between the 8-bit A register and immediate data n, and stores the result back into the A register.
 void xor_n(CPU *cpu, Memory *memory){ xor_r(cpu, memory_read(memory, cpu->pc++));}; // 0xEE
@@ -171,17 +171,20 @@ void or_l(CPU *cpu, Memory *memory){or_r(cpu, cpu->l);}; // 0xB5
 void or_a(CPU *cpu, Memory *memory){or_r(cpu, cpu->a);}; // 0xB7
 
 // Performs a bitwise OR operation between the 8-bit A register and data from the absolute address specified by the 16-bit register HL, and stores the result back into the A register.
-void or_hl(CPU *cpu, Memory *memory){ or_r(cpu, memory_read(memory, cpu->pc++));}; // 0xB6
+void or_hl(CPU *cpu, Memory *memory){ or_r(cpu, memory_read(memory, cpu->hl));}; // 0xB6
 
 // Performs a bitwise OR operation between the 8-bit A register and immediate data n, and stores the result back into the A register.
 void or_n(CPU *cpu, Memory *memory){ or_r(cpu, memory_read(memory, cpu->pc++));}; // 0xF6
 
 
 
-// Complements the A register (bitwise NOT operation).
-void cp_r(CPU *cpu, Memory *memory){
-    cpu->a = ~cpu->a;
-    cpu->f |= N_FLAG | H_FLAG; // Set N and H flags
+// Subtracts from the 8-bit A register, the 8-bit register r, and updates flags based on the result. This instruction is basically identical to SUB r, but does not update the A register.
+void cp_r(CPU *cpu, u8 r){
+    u8 result = cpu->a - r; 
+    cpu->f = N_FLAG; // Set N flag
+    if((cpu->a & 0x0F) < (r & 0x0F)) cpu->f |= H_FLAG; // Set H flag if there is a borrow from bit 4
+    if(cpu->a < r) cpu->f |= C_FLAG; // Set C flag if there is a borrow from bit 8
+    if(result == 0) cpu->f |= Z_FLAG; // Set Z flag if result is zero
 }
 
 void cp_b(CPU *cpu, Memory *memory){cp_r(cpu, cpu->b);}; // 0xB8
@@ -193,7 +196,7 @@ void cp_l(CPU *cpu, Memory *memory){cp_r(cpu, cpu->l);}; // 0xBD
 void cp_a(CPU *cpu, Memory *memory){cp_r(cpu, cpu->a);}; // 0xBF
 
 // Subtracts from the 8-bit A register, data from the absolute address specified by the 16-bit register HL, and updates flags based on the result. This instruction is basically identical to SUB (HL), but does not update the A register
-void cp_hl(CPU *cpu, Memory *memory){ cp_r(cpu, memory_read(memory, cpu->pc++));}; // 0xBE
+void cp_hl(CPU *cpu, Memory *memory){ cp_r(cpu, memory_read(memory, cpu->hl));}; // 0xBE
 
 // Subtracts from the 8-bit A register, the immediate data n, and updates flags based on the result. This instruction is basically identical to SUB n, but does not update the A register.
 void cp_n(CPU *cpu, Memory *memory){ cp_r(cpu, memory_read(memory, cpu->pc++));}; // 0xFE
@@ -208,40 +211,77 @@ void inc_r(CPU *cpu, u8 *r){
     if(*r == 0) cpu->f |= Z_FLAG; // Set Z flag if result is zero
 }
 
-void inc_b(CPU *cpu, Memory *memory); // 0x04
-void inc_c(CPU *cpu, Memory *memory); // 0x0C
-void inc_d(CPU *cpu, Memory *memory); // 0x14
-void inc_e(CPU *cpu, Memory *memory); // 0x1C
-void inc_h(CPU *cpu, Memory *memory); // 0x24
-void inc_l(CPU *cpu, Memory *memory); // 0x2C
-void inc_a(CPU *cpu, Memory *memory); // 0x3C
+void inc_b(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->b);}; // 0x04
+void inc_c(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->c);}; // 0x0C
+void inc_d(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->d);}; // 0x14
+void inc_e(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->e);}; // 0x1C
+void inc_h(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->h);}; // 0x24
+void inc_l(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->l);}; // 0x2C
+void inc_a(CPU *cpu, Memory *memory){inc_r(cpu, &cpu->a);}; // 0x3C
 
 // Increments the data at the memory address specified by the 16-bit HL register pair.
-void inc_hl(CPU *cpu, Memory *memory); // 0x34
+void inc_hl(CPU *cpu, Memory *memory){ 
+  cpu->f &= ~N_FLAG; // Clear N flag
+  u8 data = memory_read(memory, cpu->hl);
+  if((data & 0x0F) == 0x0F) cpu->f |= H_FLAG; // Set H flag if there is a carry from bit 3
+  data += 1;
+  memory_write(memory, cpu->hl, data);
+  if(data == 0) cpu->f |= Z_FLAG; // Set Z flag if result is zero
+}; // 0x33
 
 // Decrements the 8-bit register r by 1.
-void dec_b(CPU *cpu, Memory *memory); // 0x05
-void dec_c(CPU *cpu, Memory *memory); // 0x0D
-void dec_d(CPU *cpu, Memory *memory); // 0x15
-void dec_e(CPU *cpu, Memory *memory); // 0x1D
-void dec_h(CPU *cpu, Memory *memory); // 0x25
-void dec_l(CPU *cpu, Memory *memory); // 0x2D
-void dec_a(CPU *cpu, Memory *memory); // 0x3D
+void dec_r(CPU *cpu, u8 *r){
+    cpu->f |= N_FLAG; 
+    if((*r & 0x0F) == 0x00) cpu->f |= H_FLAG; 
+    else cpu->f &= ~H_FLAG;
+    (*r)--;
+    if(*r == 0) cpu->f |= Z_FLAG; 
+    else cpu->f &= ~Z_FLAG; 
+}
+
+void dec_b(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->b);}; // 0x05
+void dec_c(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->c);}; // 0x0D
+void dec_d(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->d);}; // 0x15
+void dec_e(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->e);}; // 0x1D
+void dec_h(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->h);}; // 0x25
+void dec_l(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->l);}; // 0x2D
+void dec_a(CPU *cpu, Memory *memory){dec_r(cpu, &cpu->a);}; // 0x3D
 
 // Decrements the data at the memory address specified by the 16-bit HL register pair.
-void dec_hl(CPU *cpu, Memory *memory); // 0x35
+void dec_hl(CPU *cpu, Memory *memory){ 
+  cpu->f |= N_FLAG; // Set N flag
+  u8 data = memory_read(memory, cpu->hl);
+  if((data & 0x0F) == 0x00) cpu->f |= H_FLAG; // Set H flag if there is a carry from bit 3
+  data -= 1;
+  memory_write(memory, cpu->hl, data);
+  if(data == 0) cpu->f |= Z_FLAG; // Set Z flag if result is zero
+}; // 0x35
 
 // Decimal adjust accumulator
-void daa(CPU *cpu, Memory *memory); // 0x28
+void daa(CPU *cpu, Memory *memory){
+
+}; // 0x28
 
 // Sets the carry flag, and clears the N and H flags.
-void scf(CPU *cpu, Memory *memory); // 0x38
+void scf(CPU *cpu, Memory *memory){
+  cpu->f |= C_FLAG;
+  cpu->f &= ~N_FLAG;
+  cpu->f &= ~H_FLAG;
+}; // 0x38
 
 // Flips all the bits in the 8-bit A register, and sets the N and H flags.
-void cpl(CPU *cpu, Memory *memory); // 0x2f
+void cpl(CPU *cpu, Memory *memory){
+  cpu->a = ~cpu->a;
+  cpu->f |= N_FLAG;
+  cpu->f |= H_FLAG;
+}; // 0x2f
 
 // Flips all the bits in the 8-bit A register, and sets the N and H flags.
-void ccf(CPU *cpu, Memory *memory); // 0x3f
+void ccf(CPU *cpu, Memory *memory){
+  cpu->f ^= C_FLAG;
+  cpu->f &= ~N_FLAG;
+  cpu->f &= ~H_FLAG;
+}; // 0x3f
 
 
 
